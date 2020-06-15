941e81ce9298aa0aadcb84075ee35b8f74922cbd
carrliitos <salazar.ten80@gmail.com>
2020-06-15 14:36:48 -0500

    CS381 day 1

---
+++ week1/AreaCalculator.js
@@ -0,0 +1,7 @@
+const length = 10;
+const width = 15;
+const area = length * width;
+document.write("<p>Area Calculator</p>");		
+document.write("<p>Length : " + length + "</p>");
+document.write("<p>Width : " + width + "</p>");		
+document.write("<p>Area : " + area + "</p>");
