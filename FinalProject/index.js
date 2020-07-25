71496349a0b4b7e529fd8d94f689cacc1481f3b2
carrliitos <salazar.ten80@gmail.com>
2020-07-24 23:49:42 -0500

    cleaned up some code

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -261,14 +261,15 @@ function displayShoppingCart() {
 	}
 
 	document.querySelector(".cartItems").innerHTML = itemInfo;
-	// Update total number of items selected
 	document.querySelector(".cartSummary").innerHTML = "<strong>Total classes selected:</strong> " + max;
 	document.querySelector("#totalCredits").innerHTML = "<strong>Total Credits:</strong> " + totalCredits;
+	
 	if(totalCredits >= 18) {
 		document.querySelector("#warning").innerHTML = `<p>WARNING: You may not exceed 18 credits per semester 
-														unless you have received permission from the dean of your college.</p>
-														<p>For more information, <a href="https://www.uww.edu/aaec/faqs#ci_whatis" target="_blank">visit the FAQ!</a></p>`;
+								unless you have received permission from the dean of your college.</p>
+								<p>For more information, <a href="https://www.uww.edu/aaec/faqs#ci_whatis" target="_blank">visit the FAQ!</a></p>`;
 	}
+	
 	document.querySelector("#shoppingcart").style.visibility = 'visible';
 }
 
